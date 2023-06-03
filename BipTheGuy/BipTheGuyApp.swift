c42a27c5f163c9df5e3646eb06c79b3fb5511e9a
Daniel <daniel.benet@outlook.es>
2023-06-03 18:08:26 +0200

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Dani Benet on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
