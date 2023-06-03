1485ac15c169f532a0d8404f8ad829c209f80820
Daniel <daniel.benet@outlook.es>
2023-06-03 18:40:58 +0200

    Use withAnimation to change the scale effect of the clown

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -11,6 +11,7 @@ import AVFAudio
 struct ContentView: View {
     
     @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
     
     var body: some View {
         VStack {
@@ -20,9 +21,16 @@ struct ContentView: View {
             Image("clown")
                 .resizable()
                 .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
                 .onTapGesture {
                     playSound(soundName: "punchSound")
+                    animateImage = false // will immediately shring to 90% of it's size
+                    
+                    withAnimation (.spring(response: 0.3, dampingFraction: 0.3)) {
+                        animateImage = true
+                    }
                 }
+                
             
             Spacer()
             
